b4aeab6af4b7df1e2c1399709c10693f299e468b
ExodiusLux <dplascencia2019@gmail.com>
2023-11-02 14:30:05 -0700

    Update DepthSense_OpenCV_mix.py

--- Obstacle Avoidance/code/ZED Obstacle Avoidance/DepthSense_OpenCV_mix.py
+++ Obstacle Avoidance/code/ZED Obstacle Avoidance/DepthSense_OpenCV_mix.py
@@ -11,8 +11,11 @@ if possible use depth mapping calculating to calculate the center of that contou
 
 
 """
-
-
+def Ecludian_Distance(point_cloud_value):
+    #calculating distance using euclidean distance formula: sqrt(x^2 + y^2 + z^2) at center of camera
+    return math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
+                        point_cloud_value[1] * point_cloud_value[1] +
+                        point_cloud_value[2] * point_cloud_value[2])
 def main():
     # Create a ZED camera object
     zed = sl.Camera()
@@ -61,12 +64,6 @@ def main():
             # Retrieve colored point cloud. Point cloud is aligned on the left image.
             zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
 
-            # Get and print distance value in m at the center of the image
-            # We measure the distance camera - object using Euclidean distance
-            x = round(image.get_width() / 2)
-            y = round(image.get_height() / 2)
-            err, point_cloud_value = point_cloud.get_value(x, y)
-
             # Retrieve the left image, depth image in the half-resolution (OPENCV)
             zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, res)
             zed.retrieve_image(depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, res)
@@ -78,38 +75,60 @@ def main():
             image_ocv = image_zed.get_data()
             depth_image_ocv = depth_image_zed.get_data()
 
+            #Contour Area
+            gray_frame = cv2.cvtColor(depth_image_ocv, cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(gray_frame, 240, 255, cv2.THRESH_BINARY)
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            valid_contours = [contour for contour in contours if cv2.contourArea(contour) > 1500]
+            largest_contour = max(valid_contours, key=cv2.contourArea, default=None)
+            centers = []
+            if largest_contour is not None:
+                M = cv2.moments(largest_contour)
+                if M["m00"] != 0:
+                    center_x = int(M["m10"] / M["m00"])
+                    center_y = int(M["m01"] / M["m00"])
+                    centers.append((center_x, center_y))
+                    cv2.circle(depth_image_ocv, (center_x, center_y), 10, (0, 0, 255), -1)
+                    err, point_cloud_value = point_cloud.get_value(center_x, center_y)
+                    distance = Ecludian_Distance(point_cloud_value)
+                    if not np.isnan(distance) and not np.isinf(distance):
+                        # distance * 3.28084 is used for conversion of m to ft
+                        print("Distance to center of contour at ({}, {}) (image center): {:1.3} ft".format(center_x, center_y, distance*3.28084), end="\r")
+                    point_cloud_np = point_cloud.get_data()
+                    point_cloud_np.dot(tr_np)
+
+            # Get and print distance value in m at the center of the image
+            # We measure the distance camera - object using Euclidean distance
+            #x = round(image.get_width() / 2)
+            #y = round(image.get_height() / 2)
+            #err, point_cloud_value = point_cloud.get_value(x, y)
+
             #these should draw a green circle on area that is being measured for distance
             #this one is for the regular camera image
-            cv2.circle(image_ocv, (round(res.width/2) , round(res.height/2)), 1, (0,255,0), 2) 
+            #cv2.circle(image_ocv, (round(res.width/2) , round(res.height/2)), 1, (0,255,0), 2) 
             #this one is for the depth image
-            cv2.circle(depth_image_ocv, (round(res.width/2) , round(res.height/2)), 1, (0,255,0), 2) 
+            #cv2.circle(depth_image_ocv, (round(res.width/2) , round(res.height/2)), 1, (0,255,0), 2) 
             
             #display normal image and depth image
             cv2.imshow("Image", image_ocv)
 
-            
+            cv2.line(depth_image_ocv, (0, res.height - 100), (res.width, res.height - 100), (0, 0, 255), 2)
             # Split camera in two
-            cv2.line(depth_image_ocv, (res.width // 2, 0), (res.width // 2, res.height), (255, 0, 0), 2)
+            cv2.line(depth_image_ocv, (res.width // 2, 0), (res.width // 2, res.height-100), (255, 0, 0), 2)
             cv2.imshow("Depth", depth_image_ocv)
 
-            #calculating distance using euclidean distance formula: sqrt(x^2 + y^2 + z^2)
-            distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
-                                point_cloud_value[1] * point_cloud_value[1] +
-                                point_cloud_value[2] * point_cloud_value[2])
+            #calculating distance using euclidean distance formula: sqrt(x^2 + y^2 + z^2) at center of camera
+            #distance = Ecludian_Distance(point_cloud_value)
             #updating point cloud
-            point_cloud_np = point_cloud.get_data()
-            point_cloud_np.dot(tr_np)
+            #point_cloud_np = point_cloud.get_data()
+            #point_cloud_np.dot(tr_np)
 
-            if not np.isnan(distance) and not np.isinf(distance):
+            #if not np.isnan(distance) and not np.isinf(distance):
                 # distance * 3.28084 is used for conversion of m to ft
-                print("Distance to Camera at ({}, {}) (image center): {:1.3} ft".format(x, y, distance*3.28084), end="\r")
-            else:
-                print("Can't estimate distance at this position.")
-                print("Your camera is probably too close to the scene, please move it backwards.\n")
+                #print("Distance to center of camera at ({}, {}) (image center): {:1.3} ft".format(x, y, distance*3.28084), end="\r")
             #press Q key to end program
             key = cv2.waitKey(10)
     cv2.destroyAllWindows()
     zed.close()
     return
-
 main()
